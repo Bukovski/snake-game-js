186e7768f154d771d8123602dbc1c21285d65a48
Bukovski <kmanankov@bk.ru>
2020-10-10 14:20:00 +0300

    add: draw a static snake food on field

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
 
 
 let lastRenderTime = 0
@@ -21,11 +22,13 @@ function main(currentTime) {
 window.requestAnimationFrame(main) // animation queue management
 
 function update() {
-	updateSnake()
+	updateSnake();
+	updateFood();
 }
 
 function draw() {
 	gameBoard.innerHTML = "";
 	
-	drawSnake(gameBoard)
+	drawSnake(gameBoard);
+	drawFood(gameBoard);
 }
